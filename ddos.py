f8a07f55f2faf587f6db70a0ac0984d212215c64
danger020 <96351748+danger020@users.noreply.github.com>
2022-01-30 18:43:46 +0500

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,21 @@
+print("    ------------------------    ")
+print("    SLOXPSD DDOS TOOL DANGER    ")
+print("    ------------------------    ")
+print("                                ")
+# URL
+URL = input("you adress here>>>\t")
+# cama ataka
+def DDOS():
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Ping = 298")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+    print ("Send ping to host " + URL +"...")
+
+while URL in URL:
+    DDOS()
